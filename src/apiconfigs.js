2bf00c0863f5d589597cf8a67d6ead250a08ee68
Rajat08021998 <82038227+Rajat08021998@users.noreply.github.com>
2023-10-28 17:42:48 +0530

    URL Change

--- src/apiconfigs.js
+++ src/apiconfigs.js
@@ -1,2 +1,2 @@
-export const API_URL_PROD = "http://13.51.207.101:8080/api/v1";
+export const API_URL_PROD = "http://16.170.253.57:8080/api/v1";
 export const API_URL_LOCAL = "http://localhost:8080/api/v1";
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,6 +4,6 @@ import socketio from "socket.io-client";
 export const socket = socketio.connect(
   process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
-    : "http://13.51.207.101:8080"
+    : "http://16.170.253.57:8080"
 );
 export const SocketContext = React.createContext();

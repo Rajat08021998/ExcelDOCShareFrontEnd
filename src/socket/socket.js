cd662f2359390df7070ec6793509255ab2d5a5e9
Rajat08021998 <82038227+Rajat08021998@users.noreply.github.com>
2023-11-18 12:44:14 +0530

    change

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,6 +4,6 @@ import socketio from "socket.io-client";
 export const socket = socketio.connect(
   process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
-    : "http://16.170.253.57:8080" //To change URL of Application
+    : "http://13.48.44.175:8080" //To change URL of Application
 );
 export const SocketContext = React.createContext();
